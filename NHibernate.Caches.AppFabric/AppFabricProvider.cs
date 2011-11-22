7d4a40c7857e14adc6fcde2e9327d14381e4e2d0
Simon Taylor <simon.taylor@spt-development.com>
2011-11-22 21:43:33 +0000

    Initial checkin of skeleton solution

---
+++ NHibernate.Caches.AppFabric/AppFabricProvider.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NHibernate.Cache;
+
+namespace NHibernate.Caches.AppFabric
+{
+    public class AppFabricProvider : ICacheProvider
+    {
+        public ICache BuildCache(string regionName, IDictionary<string, string> properties)
+        {
+            throw new NotImplementedException();
+        }
+
+        public long NextTimestamp()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Start(IDictionary<string, string> properties)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Stop()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
